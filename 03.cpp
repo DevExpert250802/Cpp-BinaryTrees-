47b49f9b2c9701092b4c930582ce5597f3daf327
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-11-01 11:50:06 +0530

    Cpp Implementation of Preorder traversal

---
+++ 03.cpp
@@ -0,0 +1,55 @@
+// Cpp Implementation of Preorder traversal
+
+#include <bits/stdc++.h>
+ using namespace std; 
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preorder(Node* root) {
+    if (root == NULL)
+        return;
+        
+    cout << root->data << " "; 
+    
+    preorder(root->left);
+    
+    preorder(root->right);
+}
+
+int main() {
+
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+   
+    preorder(root);
+
+    return 0;
+}
+
+/*  
+output: 1 2 4 5 3 6 7 
+
+      1
+     / \
+    2   3
+   / \ / \
+  4  5 6  7
+
+
+*/
