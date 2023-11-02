d2d3934ef9e20c5a83e5bab9e3b7ecc7039e8e33
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-11-02 11:53:38 +0530

    Iterative  Inorder traversal

--- 07.cpp
+++ 07.cpp
@@ -1,6 +1,20 @@
 
 //  Iterative  Inorder traversal
 
+/* approach :  Create empty vector ans , stack st to help traverse the tree iteratively & a pointer node to the root of the tree.
+
+Use a while loop that continues until the stack is not empty or the node is not null. This loop handles the traversal process.
+
+Within the loop:
+
+If node is not null, push node onto the stack then node = node->left This step goes as far left as possible in the tree.
+
+If node is null, it means you've reached the leftmost node. Pop a node from the stack, add its value to the ans vector,
+node = node->right  moves to the right subtree.
+
+Finally, return the ans vector
+*/
+
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
@@ -26,11 +40,12 @@ public:
 
 /*  output   :  4251637
 
-      1
-     / \
-    2   3
-   / \ / \
-  4  5 6  7
+
+       1
+      / \
+     2   3
+    / \ / \
+   4  5 6  7
   
 
 */
