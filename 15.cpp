e9cd096f2fd78737905edca4021c2a76bc96a193
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-11-04 00:12:28 +0530

    Symmetric Tree

---
+++ 15.cpp
@@ -0,0 +1,29 @@
+// Symmetric Tree
+
+class Solution {
+public:
+    bool f(TreeNode *root1,TreeNode *root2)  {
+    if(!root1) return !root2;
+    if(!root2) return !root1;
+    return (root1->val==root2->val) && f(root1->left,root2->right) && f(root1->right,root2->left);
+
+
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        return f(root->left,root->right);
+    }
+};
+
+/*
+  output: Symmetrical
+
+      1
+    / | \
+  2   |   2
+/  \  |  /  \
+3   4 |  4   3
+ \    |      /
+  5   |    5 
+
+  */
