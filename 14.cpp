fd8271ea11221c4248583382c78979f8e092d451
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-11-04 00:47:53 +0530

    Same Tree

--- 14.cpp
+++ 14.cpp
@@ -45,6 +45,16 @@ public:
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // Recursive Solution
 
+/*                  if (p == NULL || q == NULL)  return(p==q);
+checks if the pointer p is NULL , checks if the pointer q is NULL.
+The logical OR operator  is used to combine these conditions. If either p or q is NULL, the condition will evaluate to true.
+This means that if at least one of the trees is empty, the condition inside the if-statement will be executed.
+If the condition inside the if-statement is true (i.e., either p or q is NULL), it proceeds to the statement return (p == q);.
+In the return (p == q); part, it checks if p and q are equal by comparing their pointers. If p and q are both NULL,
+the condition p == q will be true because they are equal in this context. If both p and q are non-NULL, the condition p == q will be true In either case, it returns true.
+If p is NULL and q is not NULL, or if q is NULL and p is not NULL, the condition p == q will be false because they are not equal. In this case, it returns false.
+*/
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
