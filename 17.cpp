eddcd7ba023765ecc937abab99165ff970859ea8
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2023-11-05 09:58:50 +0530

    Path Sum

--- 17.cpp
+++ 17.cpp
@@ -1,7 +1,10 @@
 // Path Sum
-// Given the root of a binary tree and an integer targetSum, return true if 
-// the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
-// A leaf is a node with no children.
+/* 
+Given the root of a binary tree and an integer targetSum, return true if 
+ the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+ A leaf is a node with no children.
+ */
+
 
 class Solution {
 public:
