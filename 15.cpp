194872737b5c649f30b9cceb8a59b629db923d44
Devansh Khandelwal <108886695+DevExpert250802@users.noreply.github.com>
2024-08-09 18:35:42 +0530

    Symmetric Tree

--- 15.cpp
+++ 15.cpp
@@ -1,5 +1,25 @@
 // Symmetric Tree
 
+
+class Solution {
+public:
+    bool f(TreeNode *root1,TreeNode *root2)  {
+    if(root1==NULL&&root2==NULL) return true;
+    if(root1==NULL||root2==NULL||root1->val!=root2->val)return false;
+    return f(root1->left,root2->right) && f(root1->right,root2->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;//Tree is empty
+        return f(root->left,root->right);
+    }
+};
+
+
+
+
+Alter
+
+
 class Solution {
 public:
     bool f(TreeNode *root1,TreeNode *root2)  {
